6c92368ae5e4e8ed7aa0e8f581e0652787b284d5
Wenhua Chen <chenwenhua@live.com>
2020-05-04 22:12:21 -0700

    add last name

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,7 +6,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Wenhua!");
+            Console.WriteLine("Hello Chen Wenhua!");
         }
     }
 }
